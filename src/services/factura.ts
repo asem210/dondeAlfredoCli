33d6127b16b8b4c974eca4884f49848a61e4fe82
asem2140 <gustavoadrianpazos@gmail.com>
2023-08-14 12:30:39 -0500

    services update

--- src/services/factura.ts
+++ src/services/factura.ts
@@ -1,35 +1,25 @@
 import axios from "axios";
 import { FacturayData } from "../entities/factura";
 
-
-const BASE_URL = "https://kpl1jddpz7.execute-api.us-east-1.amazonaws.com";
+const BASE_URL = "https://mu44lco4c2.execute-api.us-east-1.amazonaws.com";
 
 const facturaService = {
-
-    create: async (
-        total: number,
-        fecha: string,
-        id_usuario: number,
-      ) => {
-        try {
-          const { data } = await axios({
-            url: `${BASE_URL}/factura/create`,
-            method: "post",
-            data: {
-              total: total,
-              fecha: fecha,
-              id_usuario: id_usuario,
-            },
-          });
-          return data;
-        } catch (error) {
-          return null;
-        }
-      },
-
-
-
+  create: async (total: number, fecha: string, id_usuario: number) => {
+    try {
+      const { data } = await axios({
+        url: `${BASE_URL}/factura/create`,
+        method: "post",
+        data: {
+          total: total,
+          fecha: fecha,
+          id_usuario: id_usuario,
+        },
+      });
+      return data;
+    } catch (error) {
+      return null;
+    }
+  },
 };
-
 
 export default facturaService;
--- src/services/pedido.ts
+++ src/services/pedido.ts
@@ -1,51 +1,45 @@
 import axios from "axios";
 
-
-
-const BASE_URL = "https://kpl1jddpz7.execute-api.us-east-1.amazonaws.com";
+const BASE_URL = "https://mu44lco4c2.execute-api.us-east-1.amazonaws.com";
 
 const pedidoService = {
+  create: async (
+    cantidad: number,
+    subtotal: number,
+    id_platillo: number,
+    id_factura: number
+  ) => {
+    try {
+      const { data } = await axios({
+        url: `${BASE_URL}/pedido/create`,
+        method: "post",
+        data: {
+          cantidad: cantidad,
+          subtotal: subtotal,
+          id_platillo: id_platillo,
+          id_factura: id_factura,
+        },
+      });
+      return data;
+    } catch (error) {
+      return null;
+    }
+  },
 
-    create: async (
-        cantidad: number,
-        subtotal: number,
-        id_platillo: number,
-        id_factura: number,
-      ) => {
-        try {
-          const { data } = await axios({
-            url: `${BASE_URL}/pedido/create`,
-            method: "post",
-            data: {
-                cantidad : cantidad,
-                subtotal : subtotal,
-                id_platillo : id_platillo,
-                id_factura : id_factura
-            },
-          });
-          return data;
-        } catch (error) {
-          return null;
-        }
-      },
-
-      listPorUsuario: async (id: number) => {
-        try {
-          const { data } = await axios({
-            url: `${BASE_URL}/pedido/selectPedidoPorUsuario`,
-            method: "post",
-            data: {
-              id: id,
-            },
-          });
-          return data.data;
-        } catch (error) {
-          return null;
-        }
-      },
-
-
+  listPorUsuario: async (id: number) => {
+    try {
+      const { data } = await axios({
+        url: `${BASE_URL}/pedido/selectPedidoPorUsuario`,
+        method: "post",
+        data: {
+          id: id,
+        },
+      });
+      return data.data;
+    } catch (error) {
+      return null;
+    }
+  },
 };
-
 
 export default pedidoService;
